05bd4e1ff0a8c0d2baeead33a38ad82d2f24b422
Darkness <113259616+Darknessoov@users.noreply.github.com>
2025-04-03 02:11:12 +0300

    main

---
+++ profiles.js
@@ -0,0 +1,10 @@
+const profiles = [
+    {
+        id: 1,
+        name: "Файзулин Карим Рамилевич",
+        birthDate: "2008-01-02",
+        description: "g.faj@mail.ru Возможный телефон: +79196974056 Тг: @Sshace / 5190312384",
+        image: "https://i.imgur.com/tq7LcoY.jpeg&auto=format&fit=crop&w=300&h=200&q=80&bg=1e1e1e",
+        location: "Казань"
+    }
+];
---
+++ users.js
@@ -0,0 +1,10 @@
+const users = {
+    'dark': {
+        password: 'ness',
+        isAdmin: true
+    },
+    'gothic': {
+        password: 'lord',
+        isAdmin: false
+    }
+};
